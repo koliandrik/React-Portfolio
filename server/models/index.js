90ee5a1c4dd2d42af8d7c9e1b09b6ba8f0beda1a
koliandrik <koliandrik@gmail.com>
2024-09-25 03:57:38 -0700

    restart

--- server/models/index.js
+++
@@ -1,7 +0,0 @@
-const Project = require('./Project');
-const Assignment = require('./Assignment');
-const Student = require('./Student');
-const Image = require('./Image');
-
-module.exports = { Project, Assignment, Student, Image };
-
