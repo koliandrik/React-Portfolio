6ac5da7be5eefa499b524a889c098fde22ccebd9
koliandrik <koliandrik@gmail.com>
2025-01-24 13:07:23 -0800

    outrun background

--- src/components/ShaderBackground/index.jsx
+++ src/components/ShaderBackground/index.jsx
@@ -8,271 +8,132 @@ const vertexShader = `
 `;
 
 const fragmentShader = `
-  #define S(x, y, z) smoothstep(x, y, z)
-  #define B(a, b, edge, t) S(a-edge, a+edge, t)*S(b+edge, b-edge, t)
-  #define sat(x) clamp(x,0.,1.)
-
-  #define streetLightCol vec3(1., .7, .3)
-  #define headLightCol vec3(.8, .8, 1.)
-  #define tailLightCol vec3(1., .1, .1)
-
-  #define HIGH_QUALITY
-  #define CAM_SHAKE 1.
-  #define LANE_BIAS .5
-  #define RAIN
-  //#define DROP_DEBUG
-
   uniform float iTime;
   uniform vec2 iResolution;
 
-  vec3 ro, rd;
-
-  float N(float t) {
-    return fract(sin(t*10234.324)*123423.23512);
-  }
-  vec3 N31(float p) {
-    vec3 p3 = fract(vec3(p) * vec3(.1031,.11369,.13787));
-    p3 += dot(p3, p3.yzx + 19.19);
-    return fract(vec3((p3.x + p3.y)*p3.z, (p3.x+p3.z)*p3.y, (p3.y+p3.z)*p3.x));
-  }
-  float N2(vec2 p) {
-    vec3 p3  = fract(vec3(p.xyx) * vec3(443.897, 441.423, 437.195));
-    p3 += dot(p3, p3.yzx + 19.19);
-    return fract((p3.x + p3.y) * p3.z);
+  float sun(vec2 uv, float battery) {
+    float val = smoothstep(0.3, 0.29, length(uv));
+    float bloom = smoothstep(0.7, 0.0, length(uv));
+    float cut = 3.0 * sin((uv.y + iTime * 0.2 * (battery + 0.02)) * 100.0) 
+                + clamp(uv.y * 14.0 + 1.0, -6.0, 6.0);
+    cut = clamp(cut, 0.0, 1.0);
+    return clamp(val * cut, 0.0, 1.0) + bloom * 0.6;
   }
 
-  float DistLine(vec3 ro, vec3 rd, vec3 p) {
-    return length(cross(p-ro, rd));
+  float grid(vec2 uv, float battery) {
+    vec2 size = vec2(uv.y, uv.y * uv.y * 0.2) * 0.01;
+    uv += vec2(0.0, iTime * 4.0 * (battery + 0.05));
+    uv = abs(fract(uv) - 0.5);
+    vec2 lines = smoothstep(size, vec2(0.0), uv);
+    lines += smoothstep(size * 5.0, vec2(0.0), uv) * 0.4 * battery;
+    return clamp(lines.x + lines.y, 0.0, 3.0);
   }
 
-  vec3 ClosestPoint(vec3 ro, vec3 rd, vec3 p) {
-    return ro + max(0., dot(p-ro, rd))*rd;
+  float dot2(in vec2 v) { return dot(v, v); }
+
+  float sdTrapezoid(in vec2 p, in float r1, float r2, float he) {
+    vec2 k1 = vec2(r2, he);
+    vec2 k2 = vec2(r2 - r1, 2.0 * he);
+    p.x = abs(p.x);
+    vec2 ca = vec2(p.x - min(p.x, (p.y < 0.0) ? r1 : r2), abs(p.y) - he);
+    vec2 cb = p - k1 + k2 * clamp(dot(k1 - p, k2) / dot2(k2), 0.0, 1.0);
+    float s = (cb.x < 0.0 && ca.y < 0.0) ? -1.0 : 1.0;
+    return s * sqrt(min(dot2(ca), dot2(cb)));
   }
 
-  float Remap(float a, float b, float c, float d, float t) {
-    return ((t-a)/(b-a))*(d-c)+c;
+  float sdLine(in vec2 p, in vec2 a, in vec2 b) {
+    vec2 pa = p - a, ba = b - a;
+    float h = clamp(dot(pa, ba) / dot(ba, ba), 0.0, 1.0);
+    return length(pa - ba * h);
   }
 
-  float BokehMask(vec3 ro, vec3 rd, vec3 p, float size, float blur) {
-    float d = DistLine(ro, rd, p);
-    float m = S(size, size*(1.-blur), d);
-    #ifdef HIGH_QUALITY
-    m *= mix(.7, 1., S(.8*size, size, d));
-    #endif
-    return m;
+  float sdBox(in vec2 p, in vec2 b) {
+    vec2 d = abs(p) - b;
+    return length(max(d, vec2(0))) + min(max(d.x, d.y), 0.0);
   }
 
-  float SawTooth(float t) {
-    return cos(t+cos(t))+sin(2.*t)*.2+sin(4.*t)*.02;
+  float opSmoothUnion(float d1, float d2, float k) {
+    float h = clamp(0.5 + 0.5 * (d2 - d1) / k, 0.0, 1.0);
+    return mix(d2, d1, h) - k * h * (1.0 - h);
   }
 
-  float DeltaSawTooth(float t) {
-    return 0.4*cos(2.*t)+0.08*cos(4.*t) - (1.-sin(t))*sin(t+cos(t));
-  }
+  float sdCloud(in vec2 p, in vec2 a1, in vec2 b1, in vec2 a2, in vec2 b2, float w) {
+    float lineVal1 = sdLine(p, a1, b1);
+    float lineVal2 = sdLine(p, a2, b2);
+    vec2 ww = vec2(w * 1.5, 0.0);
+    vec2 left = max(a1 + ww, a2 + ww);
+    vec2 right = min(b1 - ww, b2 - ww);
+    vec2 boxCenter = (left + right) * 0.5;
+    float boxH = abs(a2.y - a1.y) * 0.5;
+    float boxVal = sdBox(p - boxCenter, vec2(0.04, boxH)) + w;
 
-  vec2 GetDrops(vec2 uv, float seed, float m) {
-    float t = iTime+m*30.;
-    vec2 o = vec2(0.);
-    #ifndef DROP_DEBUG
-    uv.y += t*.05;
-    #endif
-    uv *= vec2(10., 2.5)*2.;
-    vec2 id = floor(uv);
-    vec3 n = N31(id.x + (id.y+seed)*546.3524);
-    vec2 bd = fract(uv);
-    vec2 uv2 = bd;
-    bd -= .5;
-    bd.y*=4.;
-    bd.x += (n.x-.5)*.6;
-    t += n.z * 6.28;
-    float slide = SawTooth(t);
-    float ts = 1.5;
-    vec2 trailPos = vec2(bd.x*ts, (fract(bd.y*ts*2.-t*2.)-.5)*.5);
-    bd.y += slide*2.;
-    #ifdef HIGH_QUALITY
-    float dropShape = bd.x*bd.x;
-    dropShape *= DeltaSawTooth(t);
-    bd.y += dropShape;
-    #endif
-    float d = length(bd);
-    float trailMask = S(-.2, .2, bd.y);
-    trailMask *= bd.y;
-    float td = length(trailPos*max(.5, trailMask));
-    float mainDrop = S(.2, .1, d);
-    float dropTrail = S(.1, .02, td);
-    dropTrail *= trailMask;
-    o = mix(bd*mainDrop, trailPos, dropTrail);
-    #ifdef DROP_DEBUG
-    if(uv2.x<.02 || uv2.y<.01) o = vec2(1.);
-    #endif
-    return o;
-  }
+    float uniVal1 = opSmoothUnion(lineVal1, boxVal, 0.05);
+    float uniVal2 = opSmoothUnion(lineVal2, boxVal, 0.05);
 
-  void CameraSetup(vec2 uv, vec3 pos, vec3 lookat, float zoom) {
-    ro = pos;
-    vec3 f = normalize(lookat-ro);
-    vec3 r = cross(vec3(0., 1., 0.), f);
-    vec3 u = cross(f, r);
-    float t = iTime;
-    vec2 offs = vec2(0.);
-    #ifdef RAIN
-    vec2 dropUv = uv;
-    #ifdef HIGH_QUALITY
-    float x = (sin(t*.1)*.5+.5)*.5;
-    x = -x*x;
-    float s = sin(x);
-    float c = cos(x);
-    mat2 rot = mat2(c, -s, s, c);
-    #ifndef DROP_DEBUG
-    dropUv = uv*rot;
-    dropUv.x += -sin(t*.1)*.5;
-    #endif
-    #endif
-    offs = GetDrops(dropUv, 1., 0.0);
-    #ifndef DROP_DEBUG
-    offs += GetDrops(dropUv*1.4, 10., 0.0);
-    #ifdef HIGH_QUALITY
-    offs += GetDrops(dropUv*2.4, 25., 0.0);
-    #endif
-    float ripple = sin(t+uv.y*3.1415*30.+uv.x*124.)*.5+.5;
-    ripple *= .005;
-    offs += vec2(ripple*ripple, ripple);
-    #endif
-    #endif
-    vec3 center = ro + f*zoom;
-    vec3 i = center + (uv.x-offs.x)*r + (uv.y-offs.y)*u;
-    rd = normalize(i-ro);
-  }
-
-  vec3 HeadLights(float i, float t) {
-    float z = fract(-t*2.+i);
-    vec3 p = vec3(-.3, .1, z*40.);
-    float d = length(p-ro);
-    float size = mix(.03, .05, S(.02, .07, z))*d;
-    float m = 0.;
-    float blur = .1;
-    m += BokehMask(ro, rd, p-vec3(.08, 0., 0.), size, blur);
-    m += BokehMask(ro, rd, p+vec3(.08, 0., 0.), size, blur);
-    #ifdef HIGH_QUALITY
-    m += BokehMask(ro, rd, p+vec3(.1, 0., 0.), size, blur);
-    m += BokehMask(ro, rd, p-vec3(.1, 0., 0.), size, blur);
-    #endif
-    float distFade = max(.01, pow(1.-z, 9.));
-    blur = .8;
-    size *= 2.5;
-    float r = 0.;
-    r += BokehMask(ro, rd, p+vec3(-.09, -.2, 0.), size, blur);
-    r += BokehMask(ro, rd, p+vec3(.09, -.2, 0.), size, blur);
-    r *= distFade*distFade;
-    return headLightCol*(m+r)*distFade;
-  }
-
-  vec3 TailLights(float i, float t) {
-    t = t*1.5+i;
-    float id = floor(t)+i;
-    vec3 n = N31(id);
-    float laneId = S(LANE_BIAS, LANE_BIAS+.01, n.y);
-    float ft = fract(t);
-    float z = 3.-ft*3.;
-    laneId *= S(.2, 1.5, z);
-    float lane = mix(.6, .3, laneId);
-    vec3 p = vec3(lane, .1, z);
-    float d = length(p-ro);
-    float size = .05*d;
-    float blur = .1;
-    float m = BokehMask(ro, rd, p-vec3(.08, 0., 0.), size, blur) +
-              BokehMask(ro, rd, p+vec3(.08, 0., 0.), size, blur);
-    #ifdef HIGH_QUALITY
-    float bs = n.z*3.;
-    float brake = S(bs, bs+.01, z);
-    brake *= S(bs+.01, bs, z-.5*n.y);
-    m += (BokehMask(ro, rd, p+vec3(.1, 0., 0.), size, blur) +
-          BokehMask(ro, rd, p-vec3(.1, 0., 0.), size, blur))*brake;
-    #endif
-    float refSize = size*2.5;
-    m += BokehMask(ro, rd, p+vec3(-.09, -.2, 0.), refSize, .8);
-    m += BokehMask(ro, rd, p+vec3(.09, -.2, 0.), refSize, .8);
-    vec3 col = tailLightCol*m*ft;
-    float b = BokehMask(ro, rd, p+vec3(.12, 0., 0.), size, blur);
-    b += BokehMask(ro, rd, p+vec3(.12, -.2, 0.), refSize, .8)*.2;
-    vec3 blinker = vec3(1., .7, .2);
-    blinker *= S(1.5, 1.4, z)*S(.2, .3, z);
-    blinker *= sat(sin(t*200.)*100.);
-    blinker *= laneId;
-    col += blinker*b;
-    return col;
-  }
-
-  vec3 StreetLights(float i, float t) {
-    float side = sign(rd.x);
-    float offset = max(side, 0.)*(1./16.);
-    float z = fract(i-t+offset);
-    vec3 p = vec3(2.*side, 2., z*60.);
-    float d = length(p-ro);
-    float blur = .1;
-    vec3 rp = ClosestPoint(ro, rd, p);
-    float distFade = Remap(1., .7, .1, 1.5, 1.-pow(1.-z,6.));
-    distFade *= (1.-z);
-    float m = BokehMask(ro, rd, p, .05*d, blur)*distFade;
-    return m*streetLightCol;
-  }
-
-  vec3 EnvironmentLights(float i, float t) {
-    float n = N(i+floor(t));
-    float side = sign(rd.x);
-    float offset = max(side, 0.)*(1./16.);
-    float z = fract(i-t+offset+fract(n*234.));
-    float n2 = fract(n*100.);
-    vec3 p = vec3((3.+n)*side, n2*n2*n2*1., z*60.);
-    float d = length(p-ro);
-    float blur = .1;
-    vec3 rp = ClosestPoint(ro, rd, p);
-    float distFade = Remap(1., .7, .1, 1.5, 1.-pow(1.-z,6.));
-    float m = BokehMask(ro, rd, p, .05*d, blur);
-    m *= distFade*distFade*.5;
-    m *= 1.-pow(sin(z*6.28*20.*n)*.5+.5, 20.);
-    vec3 randomCol = vec3(fract(n*-34.5), fract(n*4572.), fract(n*1264.));
-    vec3 col = mix(tailLightCol, streetLightCol, fract(n*-65.42));
-    col = mix(col, randomCol, n);
-    return m*col*.2;
+    return min(uniVal1, uniVal2);
   }
 
   void mainImage(out vec4 fragColor, in vec2 fragCoord) {
-    float t = iTime;
-    vec3 col = vec3(0.);
-    vec2 uv = fragCoord.xy / iResolution.xy;
-    uv -= .5;
-    uv.x *= iResolution.x/iResolution.y;
-    vec3 pos = vec3(.3, .15, 0.);
-    float bt = t * 5.;
-    float h1 = N(floor(bt));
-    float h2 = N(floor(bt+1.));
-    float bumps = mix(h1, h2, fract(bt))*.1;
-    bumps = bumps*bumps*bumps*CAM_SHAKE;
-    pos.y += bumps;
-    float lookatY = pos.y+bumps;
-    vec3 lookat = vec3(0.3, lookatY, 1.);
-    vec3 lookat2 = vec3(0., lookatY, .7);
-    lookat = mix(lookat, lookat2, sin(t*.1)*.5+.5);
-    uv.y += bumps*4.;
-    CameraSetup(uv, pos, lookat, 2.);
-    t *= .03;
-    float stp = 1./8.;
-    for(float i=0.; i<1.; i+=stp) {
-      col += StreetLights(i, t);
+    vec2 uv = (2.0 * fragCoord.xy - iResolution.xy) / iResolution.y;
+    float battery = 1.0;
+
+    float fog = smoothstep(0.1, -0.02, abs(uv.y + 0.2));
+    vec3 col = vec3(0.0, 0.1, 0.2);
+    if (uv.y < -0.2) {
+      uv.y = 3.0 / (abs(uv.y + 0.2) + 0.05);
+      uv.x *= uv.y * 1.0;
+      float gridVal = grid(uv, battery);
+      col = mix(col, vec3(1.0, 0.5, 1.0), gridVal);
+    } else {
+      float fujiD = min(uv.y * 4.5 - 0.5, 1.0);
+      uv.y -= battery * 1.1 - 0.51;
+
+      vec2 sunUV = uv;
+      vec2 fujiUV = uv;
+
+      sunUV += vec2(0.75, 0.2);
+      col = vec3(1.0, 0.2, 1.0);
+      float sunVal = sun(sunUV, battery);
+
+      col = mix(col, vec3(1.0, 0.4, 0.1), sunUV.y * 2.0 + 0.2);
+      col = mix(vec3(0.0, 0.0, 0.0), col, sunVal);
+
+      float fujiVal = sdTrapezoid(uv + vec2(-0.75 + sunUV.y * 0.0, 0.5), 1.75 + pow(uv.y * uv.y, 2.1), 0.2, 0.5);
+      float waveVal = uv.y + sin(uv.x * 20.0 + iTime * 2.0) * 0.05 + 0.2;
+      float wave_width = smoothstep(0.0, 0.01, waveVal);
+
+      col = mix(col, mix(vec3(0.0, 0.0, 0.25), vec3(1.0, 0.0, 0.5), fujiD), step(fujiVal, 0.0));
+      col = mix(col, vec3(1.0, 0.5, 1.0), wave_width * step(fujiVal, 0.0));
+      col = mix(col, vec3(1.0, 0.5, 1.0), 1.0 - smoothstep(0.0, 0.01, abs(fujiVal)));
+
+      col += mix(col, mix(vec3(1.0, 0.12, 0.8), vec3(0.0, 0.0, 0.2), clamp(uv.y * 3.5 + 3.0, 0.0, 1.0)), step(0.0, fujiVal));
+
+      vec2 cloudUV = uv;
+      cloudUV.x = mod(cloudUV.x + iTime * 0.1, 4.0) - 2.0;
+      float cloudTime = iTime * 0.5;
+      float cloudY = -0.5;
+      float cloudVal1 = sdCloud(cloudUV, 
+                                vec2(0.1 + sin(cloudTime + 140.5) * 0.1, cloudY), 
+                                vec2(1.05 + cos(cloudTime * 0.9 - 36.56) * 0.1, cloudY), 
+                                vec2(0.2 + cos(cloudTime * 0.867 + 387.165) * 0.1, 0.25 + cloudY), 
+                                vec2(0.5 + cos(cloudTime * 0.9675 - 15.162) * 0.09, 0.25 + cloudY), 0.075);
+      cloudY = -0.6;
+      float cloudVal2 = sdCloud(cloudUV, 
+                                vec2(-0.9 + cos(cloudTime * 1.02 + 541.75) * 0.1, cloudY), 
+                                vec2(-0.5 + sin(cloudTime * 0.9 - 316.56) * 0.1, cloudY), 
+                                vec2(-1.5 + cos(cloudTime * 0.867 + 37.165) * 0.1, 0.25 + cloudY), 
+                                vec2(-0.6 + sin(cloudTime * 0.9675 + 665.162) * 0.09, 0.25 + cloudY), 0.075);
+
+      float cloudVal = min(cloudVal1, cloudVal2);
+
+      col = mix(col, vec3(0.0, 0.0, 0.2), 1.0 - smoothstep(0.075 - 0.0001, 0.075, cloudVal));
+      col += vec3(1.0, 1.0, 1.0) * (1.0 - smoothstep(0.0, 0.01, abs(cloudVal - 0.075)));
     }
-    for(float i=0.; i<1.; i+=stp) {
-      float n = N(i+floor(t));
-      col += HeadLights(i+n*stp*.7, t);
-    }
-    stp = 1./32.;
-    for(float i=0.; i<1.; i+=stp) {
-      col += EnvironmentLights(i, t);
-    }
-    col += TailLights(0., t);
-    col += TailLights(.5, t);
-    col += sat(rd.y)*vec3(.6, .5, .9);
-    fragColor = vec4(col, 0.);
+
+    col += fog * fog * fog;
+    col = mix(vec3(col.r, col.r, col.r) * 0.5, col, battery * 0.7);
+
+    fragColor = vec4(col, 1.0);
   }
 
   void main() {
