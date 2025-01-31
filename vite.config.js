69eb0b4f06e91c5a5855433cf79457d10bd095d8
koliandrik <koliandrik@gmail.com>
2025-01-30 23:48:58 -0800

    build

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import jsconfigPaths from 'vite-jsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), jsconfigPaths()],
-  base: '/React-Portfolio/',
   server: {
     watch: {
       port: 3000,
