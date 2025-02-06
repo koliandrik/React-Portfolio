d357f697d4e5419128982d005c97312508416c5a
koliandrik <koliandrik@gmail.com>
2025-02-05 21:28:50 -0800

    fixed rewrite

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,23 +1,18 @@
 
-import { Box, Link, Flex, Container, Button, MenuItem, For,  } from '@chakra-ui/react';
+import { Box, Link, Flex, Container, Button, MenuItem, For  } from '@chakra-ui/react';
 
 import { FiChevronsLeft,  } from 'react-icons/fi';
 
-import { 
-    MenuContent,
- 
+import {  
     MenuRoot,
-    MenuTrigger,
 } from '@chakra-ui/react';
 
 import { 
-    DrawerActionTrigger,
     DrawerBackdrop,
     DrawerBody,
     DrawerCloseTrigger,
     DrawerContent,
     DrawerHeader,
-    DrawerFooter,
     DrawerRoot,
     DrawerTrigger,
     DrawerTitle,
@@ -25,7 +20,7 @@ import {
 
 function Nav() {
     return (
-        <Flex borderBottom='md' gap="4" justify="space-between" align="center" p="4" color="white" >
+        <Flex gap="4" justify="space-between" align="center" p="4" color="white" >
             <Container display={'flex'} justifyContent={'space-between'} alignItems={'center'} maxW={'container.xl'}> 
                 <Box as="h1" fontSize="xl">Mykola Nastyn</Box>
                 
@@ -37,11 +32,13 @@ function Nav() {
                         </Button>
                     </DrawerTrigger>
                     <DrawerContent display="flex" flexDirection="column" gap="4" align="center" justify="center">
+
                         <DrawerHeader justifyItems={'center'} borderBottomWidth="1px"> 
                             <DrawerTitle display={'flex'} justify={'center'}>Menu</DrawerTitle>
                             <DrawerCloseTrigger asChild>
                             </DrawerCloseTrigger>
                         </DrawerHeader>
+                        
                         <DrawerBody display="flex" flexDirection="column" gap="4" align="center" justify="center">
                             <MenuRoot>
                                 <For each={['Home', 'About', 'Portfolio', 'Contact']}>
@@ -51,6 +48,7 @@ function Nav() {
                                 </For>
                             </MenuRoot>
                         </DrawerBody>
+
                         <DrawerCloseTrigger />
                     </DrawerContent>
                 </DrawerRoot>
@@ -62,6 +60,7 @@ function Nav() {
                         )}
                     </For>
                 </Box>
+
             </Container>
         </Flex>
     );
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,4 +1,4 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Button, Card } from "@chakra-ui/react";
 
 
 const Portfolio = () => {
@@ -6,7 +6,7 @@ const Portfolio = () => {
         <Box as='section' p="4">
             <h1>My Portfolio</h1>
             <p>Welcome to my portfolio page. Here you can find some of my recent projects.</p>
-            {/* Add your portfolio items here */}
+            
         </Box>
 
         
