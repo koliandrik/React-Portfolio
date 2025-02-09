818542a1d412a5161bf5344cac72e462e6d8a96c
koliandrik <koliandrik@gmail.com>
2025-02-09 02:30:33 -0800

    t

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
         <Box as='section' p="4" justifyContent={'center'} alignItems={'center'} display={'flex'} flexDirection={'column'}>
             <Stack spacing={4} direction='column'  align="center" mt={4}>
                 <Heading size={"3xl"}>Welcome</Heading>
-                <Text fontSize={'xl'}>This is the landing page of my porfolio, feel free to navigate using the links on the top righ.</Text>
+                <Text fontSize={'xl'}>This is the landing page of my porfolio, feel free to navigate using the links on the top right.</Text>
             </Stack>
         </Box>
     );
